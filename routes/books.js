74d6dfb61770a2ba5b58f83d0e0566704cb7a049
Datu Beru <berudatu@gmail.com>
2021-08-27 00:22:02 +0700

    Add form edit book, add routing edit book

--- routes/books.js
+++ routes/books.js
@@ -6,5 +6,7 @@ bookRouter.get('/', Controller.listBook)
 bookRouter.get('/add', Controller.showAddBooksForm)
 bookRouter.post('/add', Controller.addBook)
 bookRouter.get('/:id', Controller.bookDetail)
+bookRouter.get('/:id/edit', Controller.editBookForm)
+bookRouter.post('/:id/edit', Controller.postEditBook)
 
 module.exports = bookRouter
